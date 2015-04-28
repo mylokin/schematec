42fd607de3d9f74737387405b0fb881fb7394a83
Andrey Gubarev <mylokin@me.com>
2015-04-28 20:03:22 +0300

    add basic thoughts on schematec

--- schematec/__init__.py
+++ schematec/__init__.py
@@ -1,1 +1,16 @@
+'''
+Schematec
+=========
+
+Schematec is a set of tools that brings some static typing into Python and
+makes input data validation easier. The purpose of this code is attempt to
+bring simplicity to applications logics using separation of data validation
+and actual data processing.
+
+Concepts
+--------
+
+Schematec separates concept of validation and concept of types casting.
+'''
+
 __version__ = '0.1.0'
