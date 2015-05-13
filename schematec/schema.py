152a34c5c7a1c611cbc63ef872fd33408ab5bc97
Andrey Gubarev <mylokin@me.com>
2015-05-13 21:26:49 +0300

    add type dependency for schema

--- schematec/schema.py
+++ schematec/schema.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import schematec.abc as abc
+import schematec.converters
 
 
 class Dictionary(object):
@@ -9,29 +10,37 @@ class Dictionary(object):
     Converters is appliyed second in orderder they defined
     Bound validators is appliyed third.
     '''
+    TYPE = schematec.converters.dictionary
 
     def __init__(self, **descriptors):
         self.descriptors = descriptors
 
     def __call__(self, data):
+        data = schematec.converters.dictionary(data)
+
         result = {}
+
         for name, descriptors in self.descriptors.items():
             unbound_validators = [v for v in descriptors if
                                   isinstance(v, abc.Validator) and not v.BINDING]
             for validator in unbound_validators:
                 validator(name, data)
+
             try:
                 value = data[name]
             except KeyError:
                 continue
+
             converters = [c for c in descriptors if isinstance(c, abc.Converter)]
             for converter in converters:
                 value = converter(value)
+
             bound_validators = [v for v in descriptors if isinstance(v, abc.Validator) and v.BINDING]
             for validator in bound_validators:
                 if isinstance(value, validator.BINDING):
                     validator(value)
             result[name] = value
+
         return result
 
 
